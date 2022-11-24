ee8223084128d7b1a11f8be41d9686721448e94d
anyquestions69 <danil_tyrenko@icloud.com>
2022-11-25 01:26:29 +0300

    sd

--- script.php
+++ script.php
@@ -1,7 +1,8 @@
 <?php
-$files = scandir('/usr/local/lib/php');
+$files = scandir('/var/www/html');
 foreach($files as $file){
     echo $file."\n";
+    echo " \n";
 }
 $homepage = file_get_contents('get.php');
 echo $homepage;
