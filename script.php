a5e4f749f2d768fa87818bdcfdd9acae6ba5b3d2
anyquestions69 <danil_tyrenko@icloud.com>
2022-11-25 01:20:58 +0300

    sd

--- script.php
+++ script.php
@@ -1,5 +1,8 @@
 <?php
 include($_SERVER['REMOTE_ADDR'].'/flag.php');
+include('/flag.php');
 $homepage = file_get_contents('https://2019-11-10-rce.ctf.su/flag.php');
+echo $homepage;
+$homepage = file_get_contents('flag.php');
 echo $homepage;
 ?>
