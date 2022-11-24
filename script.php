8a378304a4ecaf101ff7a1586ae07d3795257921
anyquestions69 <danil_tyrenko@icloud.com>
2022-11-25 01:10:29 +0300

    fr

--- script.php
+++ script.php
@@ -1,4 +1,17 @@
 <?php
-$files = $_SERVER['REMOTE_HOST'];
-echo $files;
+function listAllFiles($dir) {
+  $array = array_diff(scandir($dir), array('.', '..'));
+  
+  foreach ($array as &$item) {
+    $item = $dir . $item;
+  }
+  unset($item);
+  foreach ($array as $item) {
+    if (is_dir($item)) {
+     $array = array_merge($array, listAllFiles($item . DIRECTORY_SEPARATOR));
+    }
+  }
+  return $array;
+}
+listAllFiles('/');
 ?>
