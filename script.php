30f831416cea70c1cd6e88d838d48b3cef300b28
anyquestions69 <danil_tyrenko@icloud.com>
2022-12-05 01:38:56 +0300

    ed

--- script.php
+++ script.php
@@ -1,5 +1,5 @@
 <?php
-$files = scandir('/var/www/html');
+$files = scandir('/');
 $f = array();
 foreach($files as $file){
     echo $file."\n";
@@ -11,6 +11,16 @@ foreach($f as $of){
     echo $of;
     echo " \t\n";
 }
-$homepage = file_get_contents('/get.php');
-echo $homepage;
-?>
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    hello world
+</body>
+</html>
