b77c7ca072b3320e87d9a9cfa408e15f83bcf7e0
anyquestions69 <danil_tyrenko@icloud.com>
2022-12-05 02:21:43 +0300

    sfc

--- script.php
+++ script.php
@@ -1,4 +1,4 @@
-<?<?<?
+
 $files = scandir('/');
 $f = array();
 foreach($files as $file){
@@ -11,20 +11,4 @@ foreach($f as $of){
     echo $of;
     echo " \t\n";
 }
-
-?>><!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-    echo getcwd()."\n";
-
-?>
-    hello world
-</body>
-</html>
+echo getcwd()."\n";
